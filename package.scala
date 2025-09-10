be2345eb086ac295bcfcfc1975684e1cb5d22a00
ssxm11 <esteban.cordoba@correounivalle.edu.co>
2025-09-10 13:22:33 -0500

    main files added

---
+++ package.scala
@@ -0,0 +1,28 @@
+/* Se desea calcular el maximo de una lista de enteros positivos, no vaca.
+ Implemente las funciones maxLin, y maxIt que calculen ese valor, pero la primera
+ genere un proceso de recursion lineal, mientras la segunda genere un proceso iterativo.
+ Hay tres metodos que se proveen en List[Int] que pueden ser utiles para este ejercicio:
+ l.isEmpty: Boolean (devuelve si una lista l esta vaca)
+ l.head: Int (devuelve el primer elemento de la lista l)
+ l.tail: List[Int] (devuelve la lista sin el primer elemento l)*/
+package object FuncionesRecursivas{
+    def maxLin(l : List [Int ]): Int = {
+    1+1
+    }
+
+    def maxIt(l : List [Int ]): Int =  {
+    1+1
+    }
+
+    def movsTorresHanoi(n: Int ): BigInt = {
+    1+1
+    }
+
+    def torresHanoi(n:Int , t1:Int , t2:Int , t3:Int ): List [(Int , Int )] = {
+// Pasar n discos de la torre t1 a la torre t3 usando t2 como intermediaria
+ // Devuelve la lista de movimientos de parejas (a,b) indicando mover un disco de la torre a hacia
+ // la torre b
+    List()
+
+    }
+}
