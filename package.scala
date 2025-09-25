d4fc6cb36f2923955e46993cc85c0a3395ceb316
ssxm11 <esteban.cordoba@correounivalle.edu.co>
2025-09-25 08:10:30 -0500

    functions fixed and pruebas.sc addded

--- package.scala
+++ package.scala
@@ -1,20 +1,22 @@
 package object Comparador {
 type AlgoritmoOrd [T ] = List [T ] => ( List [T ] , Int )
 type Comparador [T ] = (T,T)=>Boolean
-def insert [T] ( e :T, l : List [T ] , comp : Comparador [T ] ) : ( List [T ] , Int ) = {
- // Caso base: lista vacía → simplemente devolvemos el elemento en una lista
-    case Nil => (List(e), 0)
+def insert[T](e: T, l: List[T], comp: Comparador[T]): (List[T], Int) = {
+  if (l.isEmpty) {
+    (List(e), 0) // caso base: lista vacía
+  } else {
+    val x = l.head
+    val xs = l.tail
 
-    // Si e debe ir antes del primer elemento
-    case x :: xs =>
-      if (comp(e, x)) {
-        (e :: l, 1) // Insertamos delante, hicimos 1 comparación
-      } else {
-        // Insertar recursivamente en la cola
-        val (newTail, c) = insert(e, xs, comp)
-        (x :: newTail, c + 1) // agregamos x al inicio y contamos la comparación actual
-      }
+    if (comp(e, x)) {
+      (e :: l, 1) // insertar delante y contar una comparación
+    } else {
+      val (newTail, c) = insert(e, xs, comp)
+      (x :: newTail, c + 1) // reconstruir lista y acumular comparaciones
+    }
+  }
 }
+
 def insertionSort [T] ( comp : Comparador [T ] ) : AlgoritmoOrd [T ] = {
 // R e c i b e una l i s t a de e l em en t o s de t i p o T y un comparador de e s o s e l em en t o s
 // y d e v u e l v e l a l i s t a ordenada y e l numero de compa rac iones r e a l i z a d a s en una p a r e j a
@@ -22,30 +24,46 @@ def insertionSort [T] ( comp : Comparador [T ] ) : AlgoritmoOrd [T ] = {
 5
 . . .
 }
-def menoresQue_noMenoresQue [T ] ( l : List [T ] , v :T, comp : Comparador [T ] ) : ( List [T ] , List [T ] , Int ) = {
-l match {
-    case Nil => (Nil, Nil, 0)  // Caso base: lista vacía
-    case x :: xs =>
-      val (menores, noMenores, c) = menoresQue_noMenoresQue(xs, v, comp)
-      if (comp(x, v)) {
-        (x :: menores, noMenores, c + 1) // x va a la lista de menores
-      } else {
-        (menores, x :: noMenores, c + 1) // x va a la lista de no menores
-      }
+def menoresQue_noMenoresQue[T](l: List[T], v: T, comp: Comparador[T]): (List[T], List[T], Int) = {
+  if (l.isEmpty) {
+    (Nil, Nil, 0) 
+  } else {
+    val x = l.head
+    val xs = l.tail
+
+    val (menores, noMenores, c) = menoresQue_noMenoresQue(xs, v, comp)
+
+    if (comp(x, v)) {
+      (x :: menores, noMenores, c + 1) // x va a la lista de menores
+    } else {
+      (menores, x :: noMenores, c + 1) // x va a la lista de no menores
+    }
   }
 }
-def quickSort [T] ( comp : Comparador [T ] ) : AlgoritmoOrd [T ] = {
-// R e c i b e una l i s t a de e l em en t o s de t i p o T y un comparador de e s o s e l em en t o s
-// y d e v u e l v e l a l i s t a ordenada y e l numero de compa rac iones r e a l i z a d a s en una p a r e j a
-// Usando e l q u i c k S o r t
-case Nil => Nil                       // Caso base: lista vacía
-  case x :: xs =>
-    // Particionar la lista en menores y mayores (según el comparador)
-    val (l1, l2) = xs.partition(e => comp(e, x))
 
-    // Ordenar recursivamente y concatenar
-    quickSort(l1, comp) ::: (x :: quickSort(l2, comp))
+def quickSort[T](comp: Comparador[T]): AlgoritmoOrd[T] = {
+  def ordenar(l: List[T]): (List[T], Int) = {
+    if (l.isEmpty) {
+      (Nil, 0) // caso base: lista vacía
+    } else {
+      val pivot = l.head
+      val xs = l.tail
+
+      
+      val (menores, noMenores, cPart) = menoresQue_noMenoresQue(xs, pivot, comp)
+
+      // ordenar recursivamente las dos partes
+      val (menoresOrd, cMenores)   = ordenar(menores)
+      val (noMenoresOrd, cNoMenores) = ordenar(noMenores)
+
+      // concatenar resultado y acumular comparaciones
+      (menoresOrd ++ (pivot :: noMenoresOrd), cPart + cMenores + cNoMenores)
+    }
+  }
+  ordenar
+
 }
+
 def comparar [T ] ( a1 : AlgoritmoOrd [T ] , a2 : AlgoritmoOrd [T ] , l : List [T ] ) : ( Int , Int ) = {
 // R e c i b e dos a l g o r i tm o s de o rdenam ien to y una l i s t a para o rd ena r
 // y d e v u e l v e una p a r e j a con e l numero de compa rac iones h e chas por a1 ,
